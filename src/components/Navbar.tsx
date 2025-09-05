3e58dfe2ac86779eac36886141ed39691111e519
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-05 14:58:35 +0800

    Navbar spacing adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,13 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 right-0 left-0 z-100">
-      <div className="max-w-7xl mx-auto bg-lightgrey/60 backdrop-blur-md px-4 py-5 flex items-center justify-between">
+      <div className="max-w-8xl mx-auto bg-lightgrey/60 backdrop-blur-md px-4 py-5 flex items-center justify-between">
         <div className="flex items-center gap-10">
-          <img src={NAVBAR_CONTENT.logo} height={39} width={104} alt="logo" />
+          <img src={NAVBAR_CONTENT.logo} height={10} width={104} alt="logo" />
           <ul className="hidden md:flex items-center gap-10">
             {NAVBAR_CONTENT.links.map((l) => (
               <li
-                className="font-semibold text-secondary transition-colors ease-in-out duration-300 hover:text-primary"
+                className="text-secondary font-semibold transition-colors ease-in-out duration-300 hover:text-primary"
                 key={l.id}
               >
                 <a href={l.link}>{l.text}</a>
@@ -26,13 +26,16 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className="hidden md:flex gap-3">
-          <button className="text-secondary px-4 py-3 cursor-pointer">
+        <div className="hidden md:flex items-center gap-5">
+          <a href="#" className="text-secondary hover:text-primary">
             Login
-          </button>
-          <button className="bg-primary px-10 py-3 text-white rounded-lg cursor-pointer">
+          </a>
+          <a
+            href="#"
+            className="bg-primary text-white py-2 text-sm px-8 rounded-lg hover:bg-blue-500 transition"
+          >
             Get Started
-          </button>
+          </a>
         </div>
         <div className="md:hidden">
           <span className="cursor-pointer" onClick={handleToggle}>
@@ -52,7 +55,7 @@ const Navbar = () => {
         }`}
       >
         <div className="overflow-hidden">
-          <div className="p-4 bg-lightgrey/60 backdrop-blur-md rounded-xl border border-gray-300 mt-2 mx-3">
+          <div className="p-4 bg-lightgrey/60 backdrop-blur-md rounded-xl border border-gray-300 mt-0 mx-3">
             <ul className="ml-3flex flex-col space-y-5">
               {NAVBAR_CONTENT.links.map((l) => (
                 <li
@@ -64,12 +67,16 @@ const Navbar = () => {
               ))}
             </ul>
             <div>
-              <button className="text-secondary block text-md mt-3 px-3 py-2 cursor-pointer">
+              <a href="#" className="hover:text-primary block my-5">
                 Login
-              </button>
-              <button className="bg-primary px-4 text-md py-2 mt-3 text-white rounded-lg cursor-pointer">
-                Get Started
-              </button>
+              </a>
+
+              <a
+                href="#"
+                className="bg-primary text-white py-2 px-4 rounded-lg hover:bg-blue-500 transition"
+              >
+                Start Free Trial
+              </a>
             </div>
           </div>
         </div>
