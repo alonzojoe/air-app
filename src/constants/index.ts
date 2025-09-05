ed0b58d56f36ad477b7df02af9f2047fe9d8ca48
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-05 10:36:18 +0800

    craete new HIGHLIGHT and TRACKING content implement app asset images corresponds to the item content

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,20 @@
 import Logo from "../assets/images/logo.png"
 import Hero from "../assets/images/hero.png"
+
 import PlayStore from "../assets/images/playstore.png"
 import AppStore from "../assets/images/appstore.png"
+
 import Icon1 from "../assets/images/icon1.png"
 import Icon2 from "../assets/images/icon2.png"
 import Icon3 from "../assets/images/icon3.png"
+
+import Share from "../assets/images/share.png"
+import Settings from "../assets/images/settings.png"
+
+import ImgLeft from "../assets/images/left.png"
+import ImgRight from "../assets/images/right.png"
+
+import Check from "../assets/images/check.png"
 
 export const NAVBAR_CONTENT = {
     logo: Logo,
@@ -39,5 +49,47 @@ export const FEATURES_CONTENT = {
         { id: 1, icon: Icon1, title: 'Innovative Solution', description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ' },
         { id: 2, icon: Icon2, title: 'Fully functional', description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ' },
         { id: 3, icon: Icon3, title: 'Suporting', description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ' }
+    ]
+}
+
+export const HIGHLIGHT_CONTENT = {
+    title: 'Air simplifies the organization of your web applications',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.',
+    items: [
+        { id: 1, icon: Share, title: 'Social Share', description: 'Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.' },
+        { id: 2, icon: Settings, title: 'Secure Integration', description: 'Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.' },
+    ],
+    imgLeft: ImgLeft
+}
+
+export const TRACKING_CONTENT = {
+    title: 'Keeping track of your expenses with this app',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.',
+    imgRight: ImgRight,
+    items: [
+        {
+            id: 1,
+            icon: Check,
+            title: 'Carefully designed',
+            description: 'Mucker plastered bugger all mate morish are eroselna culpa expedita.',
+        },
+        {
+            id: 2,
+            icon: Check,
+            title: 'Choose a App',
+            description: 'Mucker plastered bugger all mate morish are eroselna culpa expedita.',
+        },
+        {
+            id: 3,
+            icon: Check,
+            title: 'Seamless Sync',
+            description: 'Mucker plastered bugger all mate morish are eroselna culpa expedita.',
+        },
+        {
+            id: 4,
+            icon: Check,
+            title: 'User Interactive',
+            description: 'Mucker plastered bugger all mate morish are eroselna culpa expedita.',
+        }
     ]
 }
