f4f6898f1f4838c7bea1d3524f4690b4789a26f5
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-06 11:03:01 +0800

    replace container class from flex to grid control number of rows in  default, md and lg

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -2,16 +2,16 @@ import { STATS_CONTENT } from "../constants";
 const StatsSection = () => {
   return (
     <section id="stats">
-      <div className="max-w-7xl mx-auto px-3 py-2 flex flex-wrap items-center justify-between my-20">
+      <div className="max-w-7xl mx-auto px-3 py-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-20">
         {STATS_CONTENT.map((s) => (
           <div
             key={s.id}
             className="flex flex-col items-center space-y-4 text-center"
           >
-            <h2 className="text-darktext font-bold text-xl md:text-5xl">
+            <h2 className="text-darktext font-bold text-3xl md:text-5xl">
               {s.title}
             </h2>
-            <p className="text-secondary text-md md:text-2xl">{s.subTitle}</p>
+            <p className="text-secondary text-xl md:text-2xl">{s.subTitle}</p>
           </div>
         ))}
       </div>
