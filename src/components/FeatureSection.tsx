ee10672aed0c09ee0b63c2071567400d7c7aabca
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-06 11:50:03 +0800

    FeatureSection styling title and subTitle

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,5 +1,20 @@
+import { FEATURES_CONTENT } from "../constants";
+
 const FeatureSection = () => {
-  return <div>Feature Section</div>;
+  return (
+    <section id="features">
+      <div className="max-w-7xl mx-auto px-3 py-5 mt-20">
+        <div className="text-center flex flex-col space-y-7 mx-5 lg:mx-20 mb-10">
+          <h2 className="text-darktext text-3xl lg:text-5xl font-bold">
+            {FEATURES_CONTENT.title}
+          </h2>
+          <p className="text-secondary text-base">
+            {FEATURES_CONTENT.subTitle}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeatureSection;
