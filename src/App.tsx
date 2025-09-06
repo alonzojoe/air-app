30cc2c6f3d43e99c9a52770016f36120a6298448
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-06 11:11:28 +0800

    create new FeatureSection implement in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import StatsSection from "./components/StatsSection";
+import FeatureSection from "./components/FeatureSection";
+
 const App = () => {
   return (
     <main className="text-base text-secondary antialiased">
       <Navbar />
       <HeroSection />
       <StatsSection />
+      <FeatureSection />
     </main>
   );
 };
